2c0deddbc9160d7600216aa619ec78011ec9c291
Jie Meng <jmengxy@gmail.com>
2023-04-13 11:21:09 +0800

    Update start_colima.sh

--- start_colima.sh
+++ start_colima.sh
@@ -1,1 +1,2 @@
-colima start --cpu 2 --memory 8 --disk 30 --mount ~/apps:w --mount-type 9p 
+# change workspace to the directory where you want to mount to docker by -v
+colima start --cpu 2 --memory 8 --disk 30 --mount ~/workspace/:w --mount-type 9p 
